4c40baaabc4a86e99b4628237ea7e407f536e29c
Pol Lopez <pol.lopez.cano@gmail.com>
2025-11-07 16:24:14 +0100

    refactor(container): update wiring configuration setup

--- src/container.py
+++ src/container.py
@@ -5,6 +5,11 @@ from src.contexts.shared.infrastructure.container import SharedContainer
 
 
 class ApplicationContainer(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        packages=["src.contexts"],
+        auto_wire=False,
+    )
+
     shared_container = providers.Container(SharedContainer)
 
     auth_container = providers.Container(
